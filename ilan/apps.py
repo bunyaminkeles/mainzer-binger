785fb5fb5c3de5216fd2d948fc939e028aff8933
Bunyamin Keles <bkeles74@gmail.com>
2026-04-01 01:12:26 +0200

    tekrar check

--- ilan/apps.py
+++ ilan/apps.py
@@ -1,8 +1,11 @@
 from django.apps import AppConfig
+import logging
 
+logger = logging.getLogger(__name__)
 
 class IlanConfig(AppConfig):
     name = 'ilan'
 
     def ready(self):
+        logger.warning("🚀 [APP READY] İlan uygulaması başlatıldı ve sinyaller yükleniyor.")
         import ilan.signals
